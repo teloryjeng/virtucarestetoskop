508ffe791941d1344d8b388a3d5e1d7affc76256
Telo Ryjeng <146211829+teloryjeng@users.noreply.github.com>
2025-11-20 21:22:20 +0700

    Update simulasi.js

--- js/simulasi.js
+++ js/simulasi.js
@@ -684,8 +684,8 @@ function stopTubeSimulation() {
         thermometerMesh.rotationQuaternion = BABYLON.Quaternion.Identity();
     } 
     thermometerMesh.rotationQuaternion = BABYLON.Quaternion.FromEulerAngles(
-        0,              // Sumbu X (Nunduk/Dongak)
-        Math.PI,   // Sumbu Y (Putar Kiri -90 derajat)
+        -Math.PI/2,              // Sumbu X (Nunduk/Dongak)
+        -Math.PI/2,   // Sumbu Y (Putar Kiri -90 derajat)
         0               // Sumbu Z (Miring)
     ); 
 
@@ -1041,6 +1041,7 @@ createScene().then(scene => {
 });
 
 window.addEventListener("resize", () => engine.resize());
+
 
 
 
