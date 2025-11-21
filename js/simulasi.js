676dd4c6cf133b467761f054e83107a767c3ff50
Telo Ryjeng <146211829+teloryjeng@users.noreply.github.com>
2025-11-21 19:37:23 +0700

    Update simulasi.js

--- js/simulasi.js
+++ js/simulasi.js
@@ -684,9 +684,9 @@ function stopTubeSimulation() {
         thermometerMesh.rotationQuaternion = BABYLON.Quaternion.Identity();
     } 
     thermometerMesh.rotationQuaternion = BABYLON.Quaternion.FromEulerAngles(
-        Math.PI/2,              // Sumbu X (Nunduk/Dongak)
-        -Math.PI,   // Sumbu Y  (Putar Kiri -90 derajat)
-        -Math.PI              // Sumbu Z (Miring)
+        0,              // Sumbu X (Nunduk/Dongak)
+        0,   // Sumbu Y  (Putar Kiri -90 derajat)
+        0              // Sumbu Z (Miring)
     );
 
     // 4. Pastikan Terlihat & Matikan Billboard
@@ -1219,6 +1219,7 @@ createScene().then(scene => {
 });
 
 window.addEventListener("resize", () => engine.resize());
+
 
 
 
