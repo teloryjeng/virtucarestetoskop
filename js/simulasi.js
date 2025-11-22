ef50f306cf8aa567a52e8cadad7fa1b2ef451c85
Telo Ryjeng <146211829+teloryjeng@users.noreply.github.com>
2025-11-22 20:16:49 +0700

    Update simulasi.js

--- js/simulasi.js
+++ js/simulasi.js
@@ -681,7 +681,7 @@ function stopTubeSimulation() {
     
     // Atur Rotasi agar moncong termometer menghadap depan
     thermometerMesh.rotationQuaternion = null;
-    thermometerMesh.rotation = new BABYLON.Vector3(-1.5, -1, 0);
+    thermometerMesh.rotation = new BABYLON.Vector3(-1, -.5, -1.7);
 
     // 4. Pastikan Terlihat & Matikan Billboard
     findAllMeshesAndSetVisibility(thermometerMesh, true);
@@ -1213,6 +1213,7 @@ createScene().then(scene => {
 });
 
 window.addEventListener("resize", () => engine.resize());
+
 
 
 
